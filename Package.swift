60c6c7f443cc60571272c94c1248ceb519052036
Stavros Schizas <stavros.schizas@theblueground.com>
2020-11-25 13:28:10 +0200

    Drop Alamofire package dependency
    Signed-off-by: Stavros Schizas <stavros.schizas@theblueground.com>

--- Package.swift
+++ Package.swift
@@ -15,10 +15,9 @@ let package = Package(
             targets: ["ErrorHandler"]),
     ],
     dependencies: [
-         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0"))
     ],
     targets: [
-        .target(name: "ErrorHandler", dependencies: ["Alamofire"], path: "./ErrorHandler"),
+        .target(name: "ErrorHandler", dependencies: [], path: "./ErrorHandler"),
         .testTarget(name: "ErrorHandlerTests", dependencies: ["ErrorHandler"], path: "./Example/Tests")
     ],
     swiftLanguageVersions: [.v4_2, .v5]
